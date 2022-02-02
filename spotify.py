841178cf7dbd596bd77afbd72267d7ea96b9769e
N03M0TT3 <noemotte@gmail.com>
2022-02-02 14:26:59 +0100

    removed access to credentials

--- spotify.py
+++ spotify.py
@@ -9,7 +9,3 @@ results = sp.current_user_saved_tracks()
 for idx, item in enumerate(results['items']):
     track = item['track']
     print(idx, track['artists'][0]['name'], " – ", track['name'])
-
-# export SPOTIPY_CLIENT_ID='fad493c346d148b9bf130c2e67f65b55'
-# export SPOTIPY_CLIENT_SECRET='8b1fd3a82e674964a0addacaa3765ed0'
-# export SPOTIPY_REDIRECT_URI='http://localhost:8080'
