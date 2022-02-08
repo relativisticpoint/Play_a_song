d87a4abe56b19326d77424488cbbd49cef99208d
relativisticpoint <hsami272@gmail.com>
2022-02-08 21:21:34 +0100

    rajout des sys args pour le client id et le client secret

--- spotify.py
+++ spotify.py
@@ -1,9 +1,14 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-
+import sys
 scope = "user-library-read"
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+client_id = sys.argv[1]
+print("Client id : ", client_id)
+client_secret = sys.argv[2]
+print("Client secret : ", client_secret)
+redirect_uri = sys.argv[3]
+print("Redirect URI : ", redirect_uri)
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri))
 
 results = sp.current_user_saved_tracks()
 for idx, item in enumerate(results['items']):
